e5117aea1ec890623a34007debdc4cab585b0e35
meshack kimaiyo <meshackkimaiyo5@gmail.com>
2025-02-26 09:41:31 +0300

    fix:db connection

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,8 +4,7 @@ export const connectDB = async () => {
   try {
     const uri = process.env.MONGODB_URI as string;
     await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+     
     } as any);
     console.log('MongoDB connected');
   } catch (error) {
--- src/server.ts
+++ src/server.ts
@@ -22,7 +22,7 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Merry-Go-Round Backend is Running!');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`);
+  logger.info(`Server running on http://localhost:${PORT}`);
 });
